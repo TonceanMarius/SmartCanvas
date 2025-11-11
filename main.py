a581368e1401d821aab4fcb3d5c177ba0953e87c
Toncean_Marius <64437792+TonceanMarius@users.noreply.github.com>
2025-11-11 12:58:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -98,6 +98,3 @@ async def smart_choice(data: UserSchema, db: Session = Depends(get_db)):
 if __name__ == "__main__":
 
     uvicorn.run(app, host = "127.0.0.1", port = 8001)
-    refined_prompt = chatgpt()
-    if refined_prompt:
-        dalle(refined_prompt)
